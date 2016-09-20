19f06f83ae0a272ad7ed4e6e548c72f95a04a42f
TWANG006 <twang006@e.ntu.edu.sg>
2016-09-20 16:49:01 +0800

    Add FP image simulation

--- MatLab/fexpand.m
+++ MatLab/fexpand.m
@@ -1,3 +1,7 @@
+%INFO
+%   Last update: 28/07/2011, 05/10/2012, 12/12/2012
+%   Contact: mkmqian@ntu.edu.sg (Dr Qian Kemao)
+%   Copyright reserved.
 %expand f to [m n]
 %this function can be realized by padarray, but is slower
 function f=fexpand(f,mm,nn)

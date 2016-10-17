aa606b9042cef59ff99972f0db96967811b50c84
Tianyi <twang006@e.ntu.edu.sg>
2016-10-17 23:48:00 +0800

    WFT2 Algorithm All done & tested

--- Test/WFF2_CUDA_InitializationTest.cpp
+++ Test/WFF2_CUDA_InitializationTest.cpp
@@ -1,5 +1,6 @@
 //#include "gtest\gtest.h"
 //
+//#include "WFT2_CUDA.h"
 //#include "WFT2_CUDAf.h"
 //
 //TEST(WFF2_CUDA_Initialization_Single, WFF2_CUDA_Initialization)
@@ -21,6 +22,42 @@
 //	checkCudaErrors(cudaMemcpy(hyf, cuwft.m_d_yf, sizeof(cufftReal) * cuwft.m_iPaddedWidth * cuwft.m_iPaddedHeight, cudaMemcpyDeviceToHost));
 //
 //	std::ofstream out("xfyf_FromInit.csv", std::ios::out | std::ios::trunc);
+//
+//	for(int i=0; i<cuwft.m_iPaddedHeight; i++)
+//	{
+//		for(int j=0; j<cuwft.m_iPaddedWidth; j++)
+//		{
+//			out << "[" << hxf[i * cuwft.m_iPaddedWidth + j] << "-" << hyf[i * cuwft.m_iPaddedWidth + j] << "]" << ",";
+//		}
+//		out<<"\n";
+//	}
+//
+//	out.close();
+//
+//	free(hxf);
+//	free(hyf);
+//}
+//
+//
+//TEST(WFF2_CUDA_Initialization_Double, WFF2_CUDA_Initialization)
+//{
+//	int iWidth = 132;
+//	int iHeight = 132;
+//
+//	WFT_FPA::WFT::WFT2_DeviceResults z;
+//	WFT_FPA::WFT::WFT2_CUDA cuwft(
+//		iWidth, iHeight,
+//		WFT_FPA::WFT::WFT_TYPE::WFF,
+//		z);
+//
+//	cufftDoubleReal *hxf, *hyf;
+//	hxf = (cufftDoubleReal*)malloc(sizeof(cufftDoubleReal) * cuwft.m_iPaddedWidth * cuwft.m_iPaddedHeight);
+//	hyf = (cufftDoubleReal*)malloc(sizeof(cufftDoubleReal) * cuwft.m_iPaddedWidth * cuwft.m_iPaddedHeight);
+//
+//	checkCudaErrors(cudaMemcpy(hxf, cuwft.m_d_xf, sizeof(cufftDoubleReal) * cuwft.m_iPaddedWidth * cuwft.m_iPaddedHeight, cudaMemcpyDeviceToHost));
+//	checkCudaErrors(cudaMemcpy(hyf, cuwft.m_d_yf, sizeof(cufftDoubleReal) * cuwft.m_iPaddedWidth * cuwft.m_iPaddedHeight, cudaMemcpyDeviceToHost));
+//
+//	std::ofstream out("xfyf_FromInit_double.csv", std::ios::out | std::ios::trunc);
 //
 //	for(int i=0; i<cuwft.m_iPaddedHeight; i++)
 //	{
--- Test/WFR2_CUDA_Initialization_Test.cpp
+++ Test/WFR2_CUDA_Initialization_Test.cpp
@@ -1,39 +1,71 @@
-#include "gtest\gtest.h"
-
-#include "WFT2_CUDAf.h"
-
-#include <iostream>
-#include <fstream>
-
-using namespace std;
-
-TEST(WFR2_g_xg_yg, WFR2_Initialization)
-{
-	WFT_FPA::WFT::WFT2_DeviceResultsF z;
-
-	WFT_FPA::WFT::WFT2_CUDAF wfr(
-		132, 132, 
-		WFT_FPA::WFT::WFT_TYPE::WFR,
-		z);
-
-	/*cufftReal *h_g = (cufftReal*)malloc(sizeof(float) * 61 * 61);
-
-	cudaMemcpy(h_g, wfr.im_d_g, sizeof(float) * 61 * 61, cudaMemcpyDeviceToHost);
-	
-	
-	ofstream out("g.csv", std::ios::out | std::ios::trunc);
-
-	for (int i = 0; i < 61; i++)
-	{
-		for (int j = 0; j < 61; j++)
-		{
-			out << h_g[i * 61 + j]<< ",";
-		}
-		out << "\n";
-	}
-	out.close();
-
-	free(h_g);*/
-
-	cout <<wfr.m_rxxg_norm2<<", "<<wfr.m_ryyg_norm2<<", " << endl;
-}
+//#include "gtest\gtest.h"
+//
+//#include "WFT2_CUDA.h"
+//#include "WFT2_CUDAf.h"
+//
+//#include <iostream>
+//#include <fstream>
+//
+//using namespace std;
+//
+//TEST(WFR2_g_xg_yg, WFR2_Initialization)
+//{
+//	WFT_FPA::WFT::WFT2_DeviceResultsF z;
+//
+//	WFT_FPA::WFT::WFT2_CUDAF wfr(
+//		132, 132, 
+//		WFT_FPA::WFT::WFT_TYPE::WFR,
+//		z);
+//
+//	/*cufftReal *h_g = (cufftReal*)malloc(sizeof(float) * 61 * 61);
+//
+//	cudaMemcpy(h_g, wfr.im_d_g, sizeof(float) * 61 * 61, cudaMemcpyDeviceToHost);
+//	
+//	
+//	ofstream out("g.csv", std::ios::out | std::ios::trunc);
+//
+//	for (int i = 0; i < 61; i++)
+//	{
+//		for (int j = 0; j < 61; j++)
+//		{
+//			out << h_g[i * 61 + j]<< ",";
+//		}
+//		out << "\n";
+//	}
+//	out.close();
+//
+//	free(h_g);*/
+//
+//	cout <<wfr.m_rxxg_norm2<<", "<<wfr.m_ryyg_norm2<<", " << endl;
+//}
+//
+//TEST(WFR2_g_xg_yg, WFR2_Initialization_double)
+//{
+//	WFT_FPA::WFT::WFT2_DeviceResults z;
+//
+//	WFT_FPA::WFT::WFT2_CUDA wfr(
+//		132, 132, 
+//		WFT_FPA::WFT::WFT_TYPE::WFR,
+//		z);
+//
+//	/*cufftReal *h_g = (cufftReal*)malloc(sizeof(float) * 61 * 61);
+//
+//	cudaMemcpy(h_g, wfr.im_d_g, sizeof(float) * 61 * 61, cudaMemcpyDeviceToHost);
+//	
+//	
+//	ofstream out("g.csv", std::ios::out | std::ios::trunc);
+//
+//	for (int i = 0; i < 61; i++)
+//	{
+//		for (int j = 0; j < 61; j++)
+//		{
+//			out << h_g[i * 61 + j]<< ",";
+//		}
+//		out << "\n";
+//	}
+//	out.close();
+//
+//	free(h_g);*/
+//
+//	cout <<wfr.m_rxxg_norm2<<", "<<wfr.m_ryyg_norm2<<", " << endl;
+//}
